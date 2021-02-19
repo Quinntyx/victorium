16253629b914f3ead41d0829fc1bcdfc42426572
FlareStormGaming <harrisonhenry916@gmail.com>
2021-02-18 21:16:23 -0800

    Updated CraftingInjector.java
    Added shovel and injected shovel silk touch auto crafting @Override

--- src/main/java/flare/victorium/mixin/CraftingInjector.java
+++ src/main/java/flare/victorium/mixin/CraftingInjector.java
@@ -1,6 +1,7 @@
 package flare.victorium.mixin;
 
 import flare.victorium.tool.CustomPickaxeItem;
+import flare.victorium.tool.CustomShovelItem;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.ShapedRecipe;
@@ -16,8 +17,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ShapedRecipe.class)
 public class CraftingInjector {
     private static final ItemStack VICTORIUM_PICKAXE_OVERRIDE = new ItemStack(CustomPickaxeItem.VICTORIUM_PICKAXE, 1);
+    private static final ItemStack VICTORIUM_SHOVEL_OVERRIDE = new ItemStack(CustomShovelItem.VICTORIUM_SHOVEL, 1);
+
     static{
         VICTORIUM_PICKAXE_OVERRIDE.addEnchantment(Enchantments.FORTUNE, 4);
+        VICTORIUM_SHOVEL_OVERRIDE.addEnchantment(Enchantments.SILK_TOUCH, 1);
     }
 
     @Shadow
@@ -27,6 +31,8 @@ public class CraftingInjector {
     public void victorium$overrideRecipeOutput(CallbackInfoReturnable<ItemStack> cir) {
         if (id.equals(new Identifier("victorium:victorium_pickaxe")))
             cir.setReturnValue(VICTORIUM_PICKAXE_OVERRIDE);
-    }
 
+        if (id.equals(new Identifier("victorium:victorium_shovel")))
+            cir.setReturnValue(VICTORIUM_SHOVEL_OVERRIDE);
+    }
 }
